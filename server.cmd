ba8e20ecfe8905147eb27e77c98fccf93f8570d5
EduardoPlayss5 <139657857+EduardoPlayss5@users.noreply.github.com>
2023-09-23 21:33:14 -0300

    Add files via upload

---
+++ server.cmd
@@ -0,0 +1,8 @@
+@echo off
+title ---- Eaglerlauncher 2 ----
+echo You can see the Source here: http://www.github.com/EduardoPlayss5/eaglerlauncher2
+echo Starting server... (HTTP-SERVER) requires nodejs
+echo If you dont see any error below, server listening in: http://127.0.0.1:8081
+http-server -p 8081
+pause
+exit /b
