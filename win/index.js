8c36ae5764ee4479e572f891cbc87c1f64d91875
EduardoPlayss5 <139657857+EduardoPlayss5@users.noreply.github.com>
2023-09-23 21:20:01 -0300

    Add files via upload

---
+++ win/index.js
@@ -0,0 +1,30 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadURL('https://eduardoplayss5.github.io/eaglerlauncher2/index.html');
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) createWindow();
+});
